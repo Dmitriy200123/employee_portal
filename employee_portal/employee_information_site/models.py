f90d3a06fafabdc8561e4a288c74b3f5abd908c1
Dima <d.d.demin@urfu.me>
2021-03-05 19:08:27 +0500

    Created models for employee, department and position

--- employee_portal/employee_information_site/models.py
+++ employee_portal/employee_information_site/models.py
@@ -1,3 +1,34 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+class CompanyDepartment(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class EmployeePosition(models.Model):
+    name = models.CharField(max_length=50)
+    department = models.ForeignKey(CompanyDepartment, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.department.name}: {self.name}'
+
+
+class Employee(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    full_name = models.CharField(max_length=50)
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=11)
+    department = models.ForeignKey(CompanyDepartment, null=True, on_delete=models.SET_NULL)
+    position = models.ForeignKey(EmployeePosition, null=True, on_delete=models.SET_NULL)
+    is_new_employee = models.BooleanField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.full_name
