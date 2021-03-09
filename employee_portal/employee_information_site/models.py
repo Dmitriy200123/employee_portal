f9a6d349b7f07e048a7e16f3fc660a0fda6a0257
Nikita <buguev.nikita@gmail.com>
2021-03-09 11:41:54 +0500

    Added Candidate model

--- employee_portal/employee_information_site/models.py
+++ employee_portal/employee_information_site/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+class Candidate(models.Model):
+    entryDate = models.DateField()
+    fullName = models.CharField(max_length=50)
+    email = models.EmailField()
+    otherContacts = models.CharField(max_length=100)
+    position = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.fullName
