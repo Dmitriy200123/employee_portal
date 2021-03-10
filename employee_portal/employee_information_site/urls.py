65836df9472c9e433ab8a7851a17c383a5b3fd70
Ivan <chek539camo@gmail.com>
2021-03-10 18:05:55 +0500

    Created profile form, profile.html and profile_edit.html. Added to views ProfilePageView and ProfileEditPageView classes at employee_information_site. Added to views at authentication auto login and redirect to profile after registration

--- employee_portal/employee_information_site/urls.py
+++ employee_portal/employee_information_site/urls.py
@@ -5,4 +5,6 @@ from . import views
 app_name = 'employee_information_site'
 urlpatterns = [
     path('', views.HomePageView.as_view(), name='home_page'),
+    path('profile/', views.ProfilePageView.as_view(), name='profile'),
+    path('profile_edit/', views.ProfileEditPageView.as_view(), name="profile_edit")
 ]
--- employee_portal/employee_information_site/views.py
+++ employee_portal/employee_information_site/views.py
@@ -1,8 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import TemplateView
+from .forms import ProfileForm
+from .models import Employee
 
 
 # Create your views here.
 
+
 class HomePageView(TemplateView):
     template_name = 'employee_information_site/home_page.html'
+
+
+class ProfilePageView(TemplateView):
+    template_name = 'employee_information_site/profile.html'
+
+    def get(self, request, *args, **kwargs):
+        employee = Employee.objects.filter(user=request.user.id)
+        if not employee:
+            return redirect('employee_information_site:profile_edit')
+        context = {'employee': employee.first()}
+        return render(request, self.template_name, context)
+
+
+class ProfileEditPageView(TemplateView):
+    template_name = 'employee_information_site/profile_edit.html'
+
+    def get(self, request, *args, **kwargs):
+        employee = Employee.objects.filter(user=request.user.id)
+        if employee:
+            form = ProfileForm(instance=employee.first())
+        else:
+            form = ProfileForm()
+        context = {'form': form}
+        return render(request, self.template_name, context)
+
+    def post(self, request, *args, **kwargs):
+        employee = Employee.objects.filter(user=request.user.id)
+        if employee:
+            form = ProfileForm(request.POST, request.FILES, initial={'user': request.user.id},
+                               instance=employee.first())
+        else:
+            form = ProfileForm(request.POST, request.FILES, initial={'user': request.user.id})
+        form.fields['user'].disabled = True
+        if form.is_valid():
+            form.save()
+            return redirect('employee_information_site:profile')
+
+        return render(request, self.template_name, {'form': form})
