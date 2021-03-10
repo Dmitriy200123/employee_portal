86b60f722298636e62e1fe8c41b558da4dd061f1
Nikita <buguev.nikita@gmail.com>
2021-03-10 11:36:58 +0500

    Edited Candidate model and filtering for admin

--- employee_portal/employee_information_site/admin.py
+++ employee_portal/employee_information_site/admin.py
@@ -3,4 +3,11 @@ from django.contrib import admin
 # Register your models here.
 from .models import Candidate
 
-admin.site.register(Candidate)
+
+class CandidateList(admin.ModelAdmin):
+    list_display = ('fullName', 'entryDate', 'position')
+    list_filter = ['entryDate', 'position']
+    search_fields = ['fullName']
+
+
+admin.site.register(Candidate, CandidateList)
--- employee_portal/employee_information_site/models.py
+++ employee_portal/employee_information_site/models.py
@@ -3,11 +3,11 @@ from django.db import models
 
 # Create your models here.
 class Candidate(models.Model):
-    entryDate = models.DateField()
-    fullName = models.CharField(max_length=50)
+    entryDate = models.DateField(verbose_name='Entry Date')
+    fullName = models.CharField(max_length=50, verbose_name='Name')
     email = models.EmailField()
     otherContacts = models.CharField(max_length=100)
-    position = models.CharField(max_length=30)
+    position = models.CharField(max_length=30, verbose_name='Position')
 
     def __str__(self):
         return self.fullName
