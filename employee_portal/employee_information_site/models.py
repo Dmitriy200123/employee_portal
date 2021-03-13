74aea5ffdcea97d0ce7d624b7191c5011a418ca0
Dima <d.d.demin@urfu.me>
2021-03-13 18:49:36 +0500

    Added model "PersonBase" and refactoring model "Employee"

--- employee_portal/employee_information_site/models.py
+++ employee_portal/employee_information_site/models.py
@@ -20,17 +20,24 @@ class EmployeePosition(models.Model):
         return f'{self.department.name}: {self.name}'
 
 
-class Employee(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+class PersonBase(models.Model):
     full_name = models.CharField(max_length=50)
-    photo = models.ImageField(upload_to='employee_photos')
+    photo = models.ImageField(upload_to='employee_photos', default='skb_lab.jpg')
     email = models.EmailField()
-    phone_number = models.CharField(max_length=11)
+    phone_number = models.CharField(max_length=12)
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Employee(PersonBase):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     department = models.ForeignKey(CompanyDepartment, null=True, on_delete=models.SET_NULL)
     position = models.ForeignKey(EmployeePosition, null=True, on_delete=models.SET_NULL)
     is_new_employee = models.BooleanField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.full_name
