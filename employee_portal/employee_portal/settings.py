6ae857b5ba174b26c4be7e0d21340928bbffcabe
Dima <d.d.demin@urfu.me>
2021-03-26 16:35:29 +0500

    Settings for static files

--- employee_portal/employee_portal/settings.py
+++ employee_portal/employee_portal/settings.py
@@ -116,6 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 LOGIN_REDIRECT_URL = 'employee_information_site:home_page'
 LOGOUT_REDIRECT_URL = 'employee_information_site:home_page'
