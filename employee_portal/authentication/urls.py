993746d536fd137a1d4690d18d7c6d548a9e143a
user <anyatropina@mail.ru>
2021-04-01 21:13:30 +0500

    edit register.html and added register.css

--- employee_portal/authentication/urls.py
+++ employee_portal/authentication/urls.py
@@ -2,9 +2,16 @@ from django.urls import path
 
 from . import views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 app_name = 'authentication'
 urlpatterns = [
     path('register/', views.RegisterView.as_view(), name='register'),
     path('login/', views.LoginView.as_view(), name='login',),
     path('logout/', views.LogoutView.as_view(), name='logout')
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
