028e53d85f97fd1fbc3f1454b7f0f381d1dcf5bd
Dima <d.d.demin@urfu.me>
2021-04-01 22:57:38 +0500

    Added front realization

--- employee_portal/authentication/forms.py
+++ employee_portal/authentication/forms.py
@@ -6,27 +6,27 @@ from django.utils.translation import gettext, gettext_lazy as _
 class LoginForm(AuthenticationForm):
     username = forms.CharField(
         label=_("Логин"),
-        widget=forms.TextInput(attrs={'class': 'login'}),
+        widget=forms.TextInput(attrs={'class': 'login', 'placeholder': 'username'}),
     )
     password = forms.CharField(
         label=_("Пароль"),
         strip=False,
-        widget=forms.PasswordInput(attrs={'autocomplete': 'current-password', 'class': 'password'}),
+        widget=forms.PasswordInput(attrs={'autocomplete': 'current-password', 'class': 'password', 'placeholder': '*********'}),
     )
 
 
 class RegisterForm(UserCreationForm):
     username = forms.CharField(
         label=_("Логин"),
-        widget=forms.TextInput(attrs={'class': 'login'}),
+        widget=forms.TextInput(attrs={'class': 'login', 'placeholder': 'username'}),
     )
     password1 = forms.CharField(
         label=_("Пароль"),
         strip=False,
-        widget=forms.PasswordInput(attrs={'autocomplete': 'new-password', 'class': 'password1'}),
+        widget=forms.PasswordInput(attrs={'autocomplete': 'new-password', 'class': 'password1', 'placeholder': '*********'}),
     )
     password2 = forms.CharField(
         label=_("Подтверждение пароля"),
-        widget=forms.PasswordInput(attrs={'autocomplete': 'new-password', 'class': 'password2'}),
+        widget=forms.PasswordInput(attrs={'autocomplete': 'new-password', 'class': 'password2', 'placeholder': '*********'}),
         strip=False,
     )
