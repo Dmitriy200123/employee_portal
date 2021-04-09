b4e2f2ece44889f5127494c0edf6ed42cd95ba51
Nikita <buguev.nikita@gmail.com>
2021-04-09 06:56:05 +0500

    token delete

--- employee_portal/slack_bot/bot.py
+++ employee_portal/slack_bot/bot.py
@@ -1,4 +1,3 @@
-import datetime
 from apscheduler.schedulers.background import BackgroundScheduler
 from slack import WebClient, AsyncWebClient
 
@@ -6,33 +5,21 @@ from slack import WebClient, AsyncWebClient
 class ChatBot:
 
     def __init__(self):
-        self.client = WebClient('xoxb-1918468674416-1907327567825-W1FsgXG3X0rxJLYnB203FRYU')
-        self.AsyncClient = AsyncWebClient('xoxb-1918468674416-1907327567825-W1FsgXG3X0rxJLYnB203FRYU')
+        self.client = WebClient('Token_here')
         self.scheduler = BackgroundScheduler()
         self.scheduler.start()
         self.scheduler.pause()
 
-    def post_message(self, message='Hello world', channel_id='#random'):
-        if (len(self.scheduler.get_jobs()) == 0):
+    def post_message(self, message, channel_id):
+        if len(self.scheduler.get_jobs()) == 0:
             self.scheduler.pause()
         return self.client.chat_postMessage(channel=channel_id, text=message)
 
-    async def post_message_async(self, message: str, channel_id: str):
-        response = await self.AsyncClient.chat_postMessage(channel=channel_id, text=message)
-        return response
-
-    def post_scheduled_message(self, date, message='Hello world', channel_id='#random', ):
+    def post_scheduled_message(self, date, message, channel_id):
         '''
         Adding message to scheduler query
         :param date: date in format "2021-03-27 15:38:50"
         '''
-        if (len(self.scheduler.get_jobs()) == 0):
+        if len(self.scheduler.get_jobs()) == 0:
             self.scheduler.resume()
         self.scheduler.add_job(self.post_message, 'date', run_date=date, args=[message, channel_id])
-
-    # def schedule_message(self, message: str, channel_id: str, date):
-    #     return self.client.chat_scheduleMessage(channel=channel_id, post_at=date, text=message)
-    #
-    # async def schedule_message_async(self, date):
-    #     response = await self.AsyncClient.chat_scheduleMessage(channel='#random', text='async shcdeule', post_at=date)
-    #     return response
