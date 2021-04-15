447e4b3d3de2a0b219eaca0039142a3bc70184c1
Dima <d.d.demin@urfu.me>
2021-04-15 12:33:59 +0500

    Added widgets for ChatBotForm

--- employee_portal/chat_bots/forms.py
+++ employee_portal/chat_bots/forms.py
@@ -1,4 +1,4 @@
-from chat_bots.models import ChatBot, Sender
+from chat_bots.models import ChatBot, Sender, BotType
 from django import forms
 
 
@@ -6,6 +6,11 @@ class ChatBotForm(forms.ModelForm):
     class Meta:
         model = ChatBot
         fields = ('name', 'botType', 'token')
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'test'}),
+            'botType': forms.Select(choices=BotType.objects.all(), attrs={'class': 'test'}),
+            'token': forms.TextInput(attrs={'class': 'test'})
+        }
 
 
 class SenderForm(forms.ModelForm):
