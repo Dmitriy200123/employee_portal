894f235cbecdccaad268c0c510ef27c4e9bca25b
Ivan <chek539camo@gmail.com>
2021-04-27 18:57:56 +0500

    Added VacationScheduleParameters and EmployeeVacationPeriod models.

--- employee_portal/vacation_schedule/models.py
+++ employee_portal/vacation_schedule/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
+
 # Create your models here.
+from employee_information_site.models import Employee
+
+
+class VacationScheduleParameters(models.Model):
+    maxCountDays = models.IntegerField(verbose_name='Максимальное количество дней на отпуск')
+
+    def __str__(self):
+        return f'Максимальное количество дней на отпуск: {self.maxCountDays}'
+
+
+class EmployeeVacationPeriod(models.Model):
+    employeeId = models.ForeignKey(Employee, on_delete=models.CASCADE, verbose_name='Id сотрудника')
+    startDateVacation = models.DateField(verbose_name='Дата начала отпуска')
+    endDateVacation = models.DateField(verbose_name='Дата окончания отпуска')
+    vacationDays = models.IntegerField(verbose_name='Продолжительность отпуска')
+
+    def __str__(self):
+        return f'{self.employeeId}: {self.startDateVacation}-{self.endDateVacation}'
