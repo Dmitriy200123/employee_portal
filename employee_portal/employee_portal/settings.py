9340b16512aca3f2d5b952141eab037dc9712d51
Dima <d.d.demin@urfu.me>
2021-05-16 18:39:33 +0500

    Settings for static files; font, side_bar_style in main static directory

--- employee_portal/employee_portal/settings.py
+++ employee_portal/employee_portal/settings.py
@@ -118,7 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 
 LOGIN_REDIRECT_URL = 'employee_information_site:home_page'
 LOGOUT_REDIRECT_URL = 'employee_information_site:home_page'
