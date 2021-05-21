4077b38a0466ee2f2a8daa02362d885e1a159108
Nikita Buguev <buguev@tochka.com>
2021-05-21 21:17:36 +0500

    review fixes

--- employee_portal/vacation_schedule/urls.py
+++ employee_portal/vacation_schedule/urls.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.decorators import login_required
+from django.contrib.admin.views.decorators import staff_member_required
 from django.urls import path
 
 from . import views
@@ -9,6 +10,6 @@ urlpatterns = [
     path('add_vacation/<int:id>/', login_required(views.UpdateOrCreateVacationPeriod.as_view()), name='updateVacation'),
     path('add_vacation/', login_required(views.UpdateOrCreateVacationPeriod.as_view()), name='addVacation'),
     path('delete_vacation/<int:id>/', login_required(views.DeleteVacationPeriod.as_view()), name='deleteVacation'),
-    path('employee_vacation/', login_required(views.EmployeeVacationPage.as_view()), name='employeeVacationPage'),
-    path('employee_vacation/download/', login_required(views.export_vacation_xls)),
+    path('employee_vacation/', staff_member_required(views.EmployeeVacationPage.as_view()), name='employeeVacationPage'),
+    path('employee_vacation/download/', staff_member_required(views.export_vacation_xls)),
 ]
