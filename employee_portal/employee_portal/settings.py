8c60acf2632b313ec76890e49b1ca3e0654da67b
Dima <d.d.demin@urfu.me>
2021-05-29 12:48:38 +0500

    Changed db to postgresql, migrations for chat_bots

--- employee_portal/employee_portal/settings.py
+++ employee_portal/employee_portal/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'employee_portal.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'db_name',
+        'USER': 'user_name',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
