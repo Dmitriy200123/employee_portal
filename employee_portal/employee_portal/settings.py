aeb0f26d14ad5997bd95cc15959c4887489f95da
Ivan <chek539camo@gmail.com>
2021-06-01 17:38:04 +0500

    Added django-heroku and gunicorn libraries at requirements.txt. import django_heroku library at settings.py.

--- employee_portal/employee_portal/settings.py
+++ employee_portal/employee_portal/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -132,3 +133,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
